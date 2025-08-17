46b21f088681b734e520ac0d189291f3f35057d7
Haoooooooo1117 <chongyanhao1117@gmail.com>
2025-08-17 23:34:45 +0800

    solving merge conflict

--- flutter_iot_project/lib/main.dart
+++ flutter_iot_project/lib/main.dart
@@ -1,70 +1,72 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+import 'routing/app_router.dart';
+import 'routing/routes.dart';
+import 'theming/colors.dart';
+
+late String initialRoute;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.wait([
+    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+    ScreenUtil.ensureScreenSize(),
+    preloadSVGs(['assets/svgs/google_logo.svg']),
+  ]);
+
+  FirebaseAuth.instance.authStateChanges().listen((user) {
+    if (user == null || !user.emailVerified) {
+      initialRoute = Routes.loginScreen;
+    } else {
+      initialRoute = Routes.homeScreen;
+    }
+  });
+
+  runApp(MyApp(router: AppRouter()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+Future<void> preloadSVGs(List<String> paths) async {
+  for (final path in paths) {
+    final loader = SvgAssetLoader(path);
+    await svg.cache.putIfAbsent(
+      loader.cacheKey(null),
+      () => loader.loadBytes(null),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyApp extends StatelessWidget {
+  final AppRouter router;
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const MyApp({super.key, required this.router});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          title: 'Login & Signup App',
+          theme: ThemeData(
+            useMaterial3: true,
+            textSelectionTheme: const TextSelectionThemeData(
+              cursorColor: ColorsManager.mainBlue,
+              selectionColor: Color.fromARGB(188, 36, 124, 255),
+              selectionHandleColor: ColorsManager.mainBlue,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+          ),
+          onGenerateRoute: router.generateRoute,
+          debugShowCheckedModeBanner: false,
+          initialRoute: initialRoute,
+        );
+      },
     );
   }
 }
