6f5785597a516b6c9a62e64385f52051e73204bd
YXER._.ROR <yvxin.tan@gmail.com>
2025-09-02 20:02:10 +0800

    Change the fonts

--- Flutter-Bloc_Cubit-Animated-Auth-Screens-Firebase-Login-SignUp-Reset-SignWithGoogle-main/lib/theming/styles.dart
+++ Flutter-Bloc_Cubit-Animated-Auth-Screens-Firebase-Login-SignUp-Reset-SignWithGoogle-main/lib/theming/styles.dart
@@ -1,67 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'colors.dart';
 
 class TextStyles {
-  static TextStyle font24Blue700Weight = TextStyle(
-    fontSize: 24.sp,
+   static TextStyle font24Blue700Weight = const TextStyle(    
+    fontFamily: 'Georgia',
+    fontSize: 35,
     fontWeight: FontWeight.w700,
     color: ColorsManager.mainBlue,
   );
 
-  static TextStyle font14Blue400Weight = TextStyle(
+  static TextStyle font14Blue400Weight = GoogleFonts.nunitoSans(
     fontSize: 14.sp,
     fontWeight: FontWeight.w400,
     color: ColorsManager.mainBlue,
   );
 
-  static TextStyle font16White600Weight = TextStyle(
+  static TextStyle font16White600Weight = GoogleFonts.nunitoSans(
     fontSize: 16.sp,
     fontWeight: FontWeight.w600,
     color: Colors.white,
   );
-  static TextStyle font13Grey400Weight = TextStyle(
+  static TextStyle font13Grey400Weight = GoogleFonts.nunitoSans(
     fontSize: 13.sp,
     fontWeight: FontWeight.w400,
     color: ColorsManager.gray,
   );
-  static TextStyle font14Grey400Weight = TextStyle(
+  static TextStyle font14Grey400Weight = GoogleFonts.nunitoSans(
     fontSize: 14.sp,
     fontWeight: FontWeight.w400,
     color: ColorsManager.gray,
   );
-  static TextStyle font14Hint500Weight = TextStyle(
+  static TextStyle font14Hint500Weight = GoogleFonts.nunitoSans(
     fontSize: 14.sp,
     fontWeight: FontWeight.w500,
     color: ColorsManager.gray76,
   );
-  static TextStyle font14DarkBlue500Weight = TextStyle(
+  static TextStyle font14DarkBlue500Weight = GoogleFonts.nunitoSans(
     fontSize: 14.sp,
     fontWeight: FontWeight.w500,
     color: ColorsManager.darkBlue,
   );
-  static TextStyle font15DarkBlue500Weight = TextStyle(
+  static TextStyle font15DarkBlue500Weight = GoogleFonts.nunitoSans(
     fontSize: 15.sp,
     fontWeight: FontWeight.w500,
     color: ColorsManager.darkBlue,
   );
-  static TextStyle font11DarkBlue500Weight = TextStyle(
+  static TextStyle font11DarkBlue500Weight = GoogleFonts.nunitoSans(
     fontSize: 11.sp,
     fontWeight: FontWeight.w500,
     color: ColorsManager.darkBlue,
   );
-  static TextStyle font11DarkBlue400Weight = TextStyle(
+  static TextStyle font11DarkBlue400Weight = GoogleFonts.nunitoSans(
     fontSize: 11.sp,
     fontWeight: FontWeight.w400,
     color: ColorsManager.darkBlue,
   );
-  static TextStyle font11Blue600Weight = TextStyle(
+  static TextStyle font11Blue600Weight = GoogleFonts.nunitoSans(
     fontSize: 11.sp,
     fontWeight: FontWeight.w600,
     color: ColorsManager.mainBlue,
   );
-  static TextStyle font11MediumLightShadeOfGray400Weight = TextStyle(
+  static TextStyle font11MediumLightShadeOfGray400Weight = GoogleFonts.nunitoSans(
     fontSize: 11.sp,
     fontWeight: FontWeight.w400,
     color: ColorsManager.mediumLightShadeOfGray,
